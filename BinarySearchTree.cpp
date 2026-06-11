4ebcd05877ed7dc8ec5acdf1769d5455282f1884
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-06-11 15:29:01 +0700

    Mengatur pencarian node dari root

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -64,4 +64,19 @@ class BinaryTree
     //as well as the current node of its parrent
     void search(string element, Node *&parent, Node *&currentNode)
     {
-       
+    currentNode = ROOT;
+        parent = NULL;
+
+        while((currentNode != NULL) &&
+             (currentNode->info != element))
+             {
+                parent = currentNode;
+
+                if (element < currentNode->info)
+                    currentNode = currentNode->leftchild;
+                else
+                    currentNode = currentNode->rightchild;
+         }
+    }
+
+    
