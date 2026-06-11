b94d7dc331a3bdc2b5e66e0e61f6f4d7743f84bd
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-06-11 15:38:08 +0700

    Membuat switch case untuk menjalankan pilihan dari user.
    pil 1 untuk input word dan masukin ke tree
    pil 2 untuk program menjalankan inorder
    pil 3 untuk program jalanin prorder

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -148,4 +148,46 @@ int main()
 
         cout << endl;
 
+        switch (ch)
+        {
+        case '1':
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word;
+            obj.insert(word);
+            break;
+        }
+
+        case '2':
+        {
+            obj.inorder(obj.ROOT);
+            break;
+        }
+
+        case '3':
+        {
+            obj.preorder(obj.ROOT);
+            break;
+        }
         
+         case '4':
+        {
+            obj.postorder(obj.ROOT);
+            break;
+        }
+
+        case '5':
+            return 0;
+        
+        default:
+        {
+            cout << "Invalid option" << endl;
+            break;
+        }
+        }
+    }
+    return 0;
+} 
+           
+       
