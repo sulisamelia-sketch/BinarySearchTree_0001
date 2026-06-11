adbd294dc2dd3bfa6d96f88f128a50207cc28aeb
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-06-11 15:31:35 +0700

    Fungsi inorder untuk menampilkan data tree dari kiri dulu, lalu lanjut ke root lalu ke kanan. kalau root masih null berarti tree kosong kalau ptr masih ada, program bakal jalan rekursif ke kiri, tampilin info node, terus lanjut ke kanan

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -79,4 +79,20 @@ class BinaryTree
          }
     }
 
-    
+    void inorder(Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
+
+   
